e080de1b2ef1ac6e7e0270cb356593a57d4e2504
Ajmain Fayek <ajmainfayek733@gmail.com>
2024-11-30 00:58:29 +0600

    Hero section complete

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+    return (
+        <div
+            style={{
+                backgroundImage: 'url("https://i.ibb.co.com/1L7sSjt/3.png")',
+            }}
+            className="py-72 bg-cover bg-center flex justify-end"
+        >
+            <div className="text-white space-y-6 px-6 2xl:mr-48">
+                <h1 className="md:text-6xl text-3xl sm:text-5xl font-rancho font-semibold">
+                    Would you like a Cup of Delicious Coffee?
+                </h1>
+                <p className="max-w-[724px]">
+                    It's coffee time - Sip & Savor - Relaxation in every sip!
+                    Get the nostalgia back!! Your companion of every moment!!!
+                    Enjoy the beautiful moments and make them memorable.
+                </p>
+                <button
+                    className="btn rounded-sm bg-[#E3B577] text-[#242222]
+                hover:bg-transparent hover:text-white hover:border-[#fff]"
+                >
+                    Learn More
+                </button>
+            </div>
+        </div>
+    );
+};
 
-export default Hero
+export default Hero;
