55c0aef0b8d8ecf03a97ff77edae014d706d3584
Ajmain Fayek <ajmainfayek733@gmail.com>
2024-11-30 02:11:04 +0600

    Header bg positioned into center

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import img from "../assets/more/logo1.png";
 
 const Header = () => {
     return (
-        <div style={{ backgroundImage: `url('${bg}')` }} className="flex items-center gap-4 py-4 justify-center bg-cover">
+        <div style={{ backgroundImage: `url('${bg}')` }} className="flex items-center gap-4 py-4 justify-center bg-cover bg-center">
             <img className="h-16" src={img} />
             <p className="text-white md:text-6xl text-3xl sm:text-5xl font-rancho">Espresso Emporium</p>
         </div>
